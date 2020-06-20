6e3d9be87000b50aaf04f2d6a00be0ed5f970987
PaaTay2.0 <macbookpro@macbooks-mbp.myfiosgateway.com>
2020-06-20 12:17:28 -0400

    Fetch from Server

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+
 
 import {
     Card,
@@ -25,7 +27,7 @@ function RenderCampsite({ campsite }) {
     return (
         <div className="col-md-5 m-1">
             <Card>
-                <CardImg top src={campsite.image} alt={campsite.name} />
+                <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
                 <CardBody>
                     <CardText>{campsite.description} </CardText>
                 </CardBody>
